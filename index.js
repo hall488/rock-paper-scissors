d99aa1070da6f304ea53453b56dbee2202bdb453
hall488 <hall488@purdue.edu>
2023-07-12 10:18:27 -0500

    Create main game loop
    Game loop has 5 rounds. Each round asks player for input until they
    provide a valid answer. The computer's answer is called through a
    function. Then the round is played and the round output is logged to
    the console.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-game() {
-    
+function game() {
+    let playerWins, computerWins = 0;
+    let choices = ["rock", "paper", "scissors"];
+    for(let i = 0; i < 5; i++) {        
+        for(;;) {
+            let playerChoice = prompt("Select: Rock, Paper, or Scissors");
+
+            if(choices.includes(playerChoice.toLowerCase())) break;
+            else console.log("Unexpected input: Please type Rock, Paper, or Scissors!");
+        }
+        let computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice, computerChoice));
+    }
 }
