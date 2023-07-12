ca0a8da217313ec63565fcc0fbf0dfd707a7490c
hall488 <hall488@purdue.edu>
2023-07-12 10:35:03 -0500

    Add playRound function
    Function takes two inputs, player and computer choices. It returns
    a string that declares if they player won or lose.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let choices = ["rock", "paper", "scissors"];
+let playerWins, computerWins;
 
-function game() {
-    let playerWins, computerWins = 0;
+function game() {    
+    playerWins, computerWins = 0
     
     for(let i = 0; i < 5; i++) {        
         for(;;) {
@@ -17,4 +18,27 @@ function game() {
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
+}
+
+function playRound(playerChoice, computerChoice) {
+
+    console.log(`Player chose ${playerChoice}, Computer chose ${computerChoice}`);
+
+    if(playerChoice == computerChoice) {
+        return "Draw!";
+    }
+
+    if(playerChoice == "rock") {
+        return computerChoice == "paper" ? "You lose!" : "You win!";
+    }
+
+    if(playerChoice == "paper") {
+        return computerChoice == "scissors" ? "You lose!" : "You win!";
+    }
+
+    if(playerChoice == "scissors") {
+        return computerChoice == "rock" ? "You lose!" : "You win!";
+    }
+
+    return "Error: Unexpected Scenario!";
 }
